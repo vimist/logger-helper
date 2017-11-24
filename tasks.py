a97584e6fb98bb15d53fb13e2f4336f8249519ee
vimist <vimist@users.noreply.github.com>
2017-11-24 18:04:14 +0000

    Modified tasks to use build in dependency management
    tasks.py now uses the built in dependency management that invoke
    provides. This also gives us task de-duplication for free.

--- tasks.py
+++ tasks.py
@@ -10,7 +10,6 @@ DOCS_DIR = 'docs'
 CI_IMAGE = 'logger-helper-ci'
 
 IN_CI_ENVIRONMENT = os.getenv('CI', None) is not None
-BUILT_CI = False # Whether we have already build the CI image in this run
 
 
 def ci(cxt, command, flags='', pty=False):
@@ -46,34 +45,26 @@ def ci(cxt, command, flags='', pty=False):
 @task
 def build_ci(cxt):
     """Build the CI image."""
-    global BUILT_CI # Allow assignment in the global scope
-
-    if not IN_CI_ENVIRONMENT and not BUILT_CI:
+    if not IN_CI_ENVIRONMENT:
         command = 'docker build --tag {ci_image} .'
         cxt.run(command.format(ci_image=CI_IMAGE))
 
         BUILT_CI = True
 
-@task
+@task(build_ci)
 def run_ci(cxt):
     """Run the CI environment and launch a shell."""
-    build_ci(cxt)
-
     ci(cxt, 'sh', flags='-it', pty=True)
 
-@task
+@task(build_ci)
 def build_docs(cxt):
     """Build the documentation."""
-    build_ci(cxt)
-
-    command = 'make -C {docs_dir} html'
+    command = 'sphinx-build {docs_dir} {docs_dir}/_build/html'
     ci(cxt, command.format(docs_dir=DOCS_DIR))
 
-@task
+@task(build_docs)
 def serve_docs(cxt, port=8080):
     """Serve the documentation by default on port 8080."""
-    build_docs(cxt)
-
     command = 'cd "{html_dir}" && python3 -m http.server {port}'
     command = command.format(
         html_dir='{docs_dir}/_build/html'.format(docs_dir=DOCS_DIR),
@@ -81,17 +72,9 @@ def serve_docs(cxt, port=8080):
 
     cxt.run(command, pty=True)
 
-@task
-def test(cxt):
-    """Run all tests."""
-    unit_test(cxt)
-    test_install(cxt)
-
-@task
+@task(build_ci)
 def unit_test(cxt):
     """Run the unit tests."""
-    build_ci(cxt)
-
     command = 'coverage run -m unittest {tests_file}'
     ci(cxt, command.format(tests_file=TESTS_FILE))
 
@@ -100,28 +83,22 @@ def test_install(cxt):
     """Test that the package installs with PIP."""
     ci(cxt, 'pip install .')
 
-@task
-def lint(cxt):
-    """Run the linters."""
-    lint_module(cxt)
-    lint_tests(cxt)
+@task(unit_test, test_install)
+def test(cxt):
+    """Run all tests."""
 
-@task
+@task(build_ci)
 def lint_module(cxt):
     """Run the linters on the module."""
-    build_ci(cxt)
-
     command = 'pylint --reports no {module} setup.py'
     ci(cxt, command.format(module=MODULE_NAME))
 
     command = 'flake8 {module} setup.py'
     ci(cxt, command.format(module=MODULE_NAME))
 
-@task
+@task(build_ci)
 def lint_tests(cxt):
     """Run the linters on the tests."""
-    build_ci(cxt)
-
     command = (
         'pylint'
         ' --reports no'
@@ -132,3 +109,7 @@ def lint_tests(cxt):
 
     command = 'flake8 --ignore D100,D101,D102,D103,D107 {tests_file}'
     ci(cxt, command.format(tests_file=TESTS_FILE))
+
+@task(lint_module, lint_tests)
+def lint(cxt):
+    """Run the linters."""
